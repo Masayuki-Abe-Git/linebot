a92f6deb4a598c2a14613daa802c632acc082410
Masayuki-Abe-Git <mailto:abe2020reol@gmail.com>
2020-07-28 10:19:18 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -25,12 +25,12 @@ class LinebotController < ApplicationController
     events.each { |event|
       if event.message['text'] != nil
         place = event.message['text'] #ここでLINEで送った文章を取得
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3fa92180d46259a9942d63e66ffe5e11&format=json&address=#{place}`#ここでぐるなびAPIを叩く
+        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3fa92180d46259a9942d63e66ffe5e11\\&format=json&address=#{place}`#ここでぐるなびAPIを叩く
       else
         latitude = event.message['latitude']
         longitude = event.message['longitude']
 
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3fa92180d46259a9942d63e66ffe5e11&format=json&latitude=#{latitude}&longitude=#{longitude}`#ここでぐるなびAPIを叩く
+        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=3fa92180d46259a9942d63e66ffe5e11\\&format=json&latitude=#{latitude}&longitude=#{longitude}`#ここでぐるなびAPIを叩く
       end
 
       hash_result = JSON.parse result #レスポンスが文字列なのでhashにパースする
