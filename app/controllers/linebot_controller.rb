388178594bc9fe633b0bb97838e046d84a1e2701
Masayuki-Abe-Git <mailto:abe2020reol@gmail.com>
2020-07-29 17:54:40 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,12 +1,13 @@
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
-  # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback] 
+  require 'line/bot'
+
+  protect_from_forgery :except => [:callback]
+
   def client
-    @client ||= Line::Bot::Client.new { |config| 
+    @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    } 
+    }
   end
 
   def callback
@@ -14,58 +15,52 @@ class LinebotController < ApplicationController
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+      head :bad_request
     end
 
     events = client.parse_events_from(body)
 
-    #ここでlineに送られたイベントを検出している
-    # messageのtext: に指定すると、返信する文字を決定することができる
-    #event.message['text']で送られたメッセージを取得することができる
     events.each { |event|
-      if event.message['text'] != nil
-        place = event.message['text'] #ここでLINEで送った文章を取得
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=3fa92180d46259a9942d63e66ffe5e11&format=json&address=#{place}`#ここでぐるなびAPIを叩く
-      else
-        latitude = event.message['latitude']
-        longitude = event.message['longitude']
-
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=3fa92180d46259a9942d63e66ffe5e11&format=json&latitude=#{latitude}&longitude=#{longitude}`#ここでぐるなびAPIを叩く
-      end
-
-      hash_result = JSON.parse result #レスポンスが文字列なのでhashにパースする
-      shops = hash_result["rest"] #ここでお店情報が入った配列となる
-      shop = shops.sample #任意のものを一個選ぶ
-
-      #店の情報
-      url = shop["url_mobile"] #サイトのURLを送る
-      shop_name = shop["name"] #店の名前
-      category = shop["category"] #カテゴリー
-      open_time = shop["opentime"] #空いている時間
-      holiday = shop["holiday"] #定休日
-
-      if open_time.class != String #空いている時間と定休日の二つは空白の時にHashで返ってくるので、文字列に直そうとするとエラーになる。そのため、クラスによる場合分け。
-        open_time = ""
-      end
-      if holiday.class != String
-        holiday = ""
-      end
-
-      response = "【店名】" + shop_name + "\n" + "【カテゴリー】" + category + "\n" + "【営業時間と定休日】" + open_time + "\n" + holiday + "\n" + url
-       case event #case文caseの値がwhenと一致する時にwhenの中の文章が実行される(switch文みたいなもの)
+      case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::MessageType::Text,Line::Bot::Event::MessageType::Location
-          message = {
-            type: 'text',
-            text: response
-          }
-          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Text
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
         end
-
       end
-    } 
+    }
 
     head :ok
+  end
+
+  private
+
+  def template
+    {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "今日のもくもく会は楽しいですか？",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "楽しい",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "楽しい"
+              },
+              {
+                "type": "message",
+                "label": "楽しくない",
+                "text": "楽しくない"
+              }
+          ]
+      }
+    }
   end
 end
